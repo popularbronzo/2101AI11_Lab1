6fc70a99c285194c0b84cb1d04904cd1e4275a9e
biswajit <bs7063009327@gmail.com>
2022-07-27 21:48:21 +0530

    Bubble sort

--- sorting.c
+++ sorting.c
@@ -56,7 +56,56 @@ void bubbleSort(int *A, int n){
         }  
     } 
 }
+void merge(int A[], int mid, int low, int high)
+{
+    int i, j, k, B[100];
+    i = low;
+    j = mid + 1;
+    k = low;
 
+    while (i <= mid && j <= high)
+    {
+        if (A[i] < A[j])
+        {
+            B[k] = A[i];
+            i++;
+            k++;
+        }
+        else
+        {
+            B[k] = A[j];
+            j++;
+            k++;
+        }
+    }
+    while (i <= mid)
+    {
+        B[k] = A[i];
+        k++;
+        i++;
+    }
+    while (j <= high)
+    {
+        B[k] = A[j];
+        k++;
+        j++;
+    }
+    for (int i = low; i <= high; i++)
+    {
+        A[i] = B[i];
+    }
+    
+}
+
+void mergeSort(int A[], int low, int high){
+    int mid; 
+    if(low<high){
+        mid = (low + high) /2;
+        mergeSort(A, low, mid);
+        mergeSort(A, mid+1, high);
+        merge(A, mid, low, high);
+    }
+}
 
 int main()
 {
@@ -87,7 +136,10 @@ int main()
              bubbleSort(A,n);
              printArray(A, n);
              break;
-        
+         case 4:
+            mergeSort(A,0,n-1);
+             printArray(A, n);
+             break;
 
     };
     
